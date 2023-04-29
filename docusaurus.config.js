497d9e41fd6752e012ff235ec9bb3f9ba918e1d4
Asharam Seervi <seerviasharam@gmail.com>
2023-04-29 12:59:34 +0530

    Chore: plugin-sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,12 @@ const config = {
           trackingID: 'G-S9KBTW5Q32',
           anonymizeIP: true,
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
       }),
     ],
   ],
