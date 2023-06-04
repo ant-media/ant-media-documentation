d7e29497adc7db31275f2601f37d387dec950232
timantmedia <tim.bobker@antmedia.io>
2023-06-04 23:00:44 +0100

    try fix issue

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -8,7 +8,7 @@ export default function VideoPlayer({children, video, youtube, css}){
           <>
             <iframe className="border border-rounded m-3" width="100%" height="100%" src={video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             {children}
-            <br/><br/><br/>
+            <br/><br/>
           </>
         )
     } else {
