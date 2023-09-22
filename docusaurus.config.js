cef9d6cfc891f76e20a8a0f5cf143f40b6e13b88
timantmedia <tim.bobker@antmedia.io>
2023-09-22 14:16:33 +0100

    remove redocusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,23 +59,6 @@ const config = {
         },
       }),
     ],
-    [
-      'redocusaurus',
-      {
-        // Plugin Options for loading OpenAPI files
-        specs: [
-          {
-            spec: 'openapi.yaml',
-            route: '/rest-api/',
-          },
-        ],
-        // Theme Options for modifying how redoc renders them
-        theme: {
-          // Change with your site colors
-          primaryColor: '#1890ff',
-        },
-      },
-    ],
   ],
 
   plugins: [[ require.resolve('docusaurus-lunr-search'), {
