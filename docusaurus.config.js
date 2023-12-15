0d4862ba2f92fc008d1ca9b5d4ece6b24a319c6e
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-12-15 08:28:07 +0300

    Update announce bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -254,7 +254,7 @@ const config = {
       announcementBar: {
         id: 'announcement-bar',
         content:
-          `<iframe id="black-friday-banner" src="https://antmedia.io/docs/html/bf.html" width="100%" height="90px"></iframe>
+          `<iframe id="hackathon-banner" src="https://antmedia.io/docs/html/hackathonBanner.html" width="100%" height="90px"></iframe>
           <div style="font-size:16px;">
           <a target="_blank" href="https://antmedia.io/create-your-own-streaming-service-on-aws-in-5-minutes/">
             <strong>How to Get Your Own Auto Scalable Streaming Service on AWS in 5 minutes?</strong>
