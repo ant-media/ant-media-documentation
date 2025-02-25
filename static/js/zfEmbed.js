736d5126852111d9e4185abc7573f1ac313ebf9d
yashtandon113 <82374739+yashtandon113@users.noreply.github.com>
2025-02-26 01:18:57 +0530

    Create zfEmbed.js

---
+++ static/js/zfEmbed.js
@@ -0,0 +1,7 @@
+window._zfQueue = window._zfQueue || [];
+function _zf() { _zfQueue.push(arguments); }
+(function() {
+  ZonkaFeedback = function (en, fb) {
+    document.body.addEventListener(en, fb, false);
+  };
+})();
