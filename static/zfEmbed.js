d24fad4d53049f0208b4b36985144797d3248ee0
yashtandon113 <82374739+yashtandon113@users.noreply.github.com>
2026-04-23 21:15:23 +0530

    Update zfEmbed.js

--- static/zfEmbed.js
+++ static/zfEmbed.js
@@ -1,7 +1,26 @@
-window._zfQueue = window._zfQueue || []; function _zf(){_zfQueue.push(arguments); }
-      (function() { ZonkaFeedback = function (en, fb ){document.body.addEventListener(en, fb ,
-      false); }; var sc, w, d = document, ce = d.createElement, gi = d.getElementById, gt =
-      d.getElementsByTagName, id = "zfEmbedScript"; if (!gi.call(d, id)) { sc = ce.call(d, "script");
-      sc.async=!0;sc.id = id; sc.src = "https://us-js.zonka.co/
-      679b6f226131b4000836e0bf"; w = gt.call(d, "script")[0];
-      w.parentNode.insertBefore(sc, w); }})();
+window._zfQueue = window._zfQueue || [];
+function _zf(){_zfQueue.push(arguments); }
+
+(function() {
+  ZonkaFeedback = function (en, fb ){
+    document.body.addEventListener(en, fb, false);
+  };
+
+  var sc, w, d = document,
+      ce = d.createElement,
+      gi = d.getElementById,
+      gt = d.getElementsByTagName,
+      id = "zfEmbedScript";
+
+  if (!gi.call(d, id)) {
+    sc = ce.call(d, "script");
+    sc.async = true;
+    sc.id = id;
+
+    // ✅ FIXED LINE
+    sc.src = "https://us-js.zonka.co/679b6f226131b4000836e0bf";
+
+    w = gt.call(d, "script")[0];
+    w.parentNode.insertBefore(sc, w);
+  }
+})();
