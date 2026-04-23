ad09bc1b664c7aa72aa22cae41e2f672f35f6f44
yashtandon113 <82374739+yashtandon113@users.noreply.github.com>
2026-04-23 21:12:06 +0530

    Update zfEmbed.js

--- static/zfEmbed.js
+++ static/zfEmbed.js
@@ -1,19 +1,7 @@
-// Ensure queue exists
-window._zfQueue = window._zfQueue || [];
-
-function _zf() {
-  window._zfQueue.push(arguments);
-}
-
-// Load Zonka script (only once)
-(function () {
-  if (!document.getElementById("zfEmbedScript")) {
-    var sc = document.createElement("script");
-    sc.async = true;
-    sc.id = "zfEmbedScript";
-    sc.src = "https://us-js.zonka.co/679b6f226131b4000836e0bf";
-
-    var firstScript = document.getElementsByTagName("script")[0];
-    firstScript.parentNode.insertBefore(sc, firstScript);
-  }
-})();
+window._zfQueue = window._zfQueue || []; function _zf(){_zfQueue.push(arguments); }
+      (function() { ZonkaFeedback = function (en, fb ){document.body.addEventListener(en, fb ,
+      false); }; var sc, w, d = document, ce = d.createElement, gi = d.getElementById, gt =
+      d.getElementsByTagName, id = "zfEmbedScript"; if (!gi.call(d, id)) { sc = ce.call(d, "script");
+      sc.async=!0;sc.id = id; sc.src = "https://us-js.zonka.co/
+      679b6f226131b4000836e0bf"; w = gt.call(d, "script")[0];
+      w.parentNode.insertBefore(sc, w); }})();
